69053f01de7a041e4d01ba7f8b36a5a032a16d1a
Dominic Forster <forsterdominic2@gmail.com>
2019-09-07 21:43:44 +0200

    TODO: figure out how to test webpack with jest

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,9 @@
-const glob = require('glob');
-
-let entry = __dirname + "/src/index.js";
-let outputPath = __dirname + "/dist/";
-
-if (process.env.TESTING) {
-  entry = glob.sync(__dirname + "/tests/**/*.test.js");
-  outputPath = __dirname + "/test-dist/";
-}
 
 module.exports = {
-  entry: entry,
+  mode: 'development',
+  entry: __dirname + "/src/index.js",
   output: {
     filename: 'bundle.js',
-    path: outputPath
+    path: __dirname + "/dist/"
   },
 };
